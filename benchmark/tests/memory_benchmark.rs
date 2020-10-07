345bfbe6e8123b87c059e9fce84cd9e254fc5956
caelis <simon.ruoss@gmail.com>
2020-10-07 17:36:28 +0200

    test fix

--- benchmark/tests/memory_benchmark.rs
+++ benchmark/tests/memory_benchmark.rs
@@ -10,16 +10,23 @@ use abin_benchmark::{
 #[global_allocator]
 static GLOBAL: &StatsAlloc<System> = &INSTRUMENTED_SYSTEM;
 
+/// Makes sure that the `abin` implementation is usually(*) better than the other
+/// implementations. (*): See exceptions below.
 #[test]
 fn test_abin_is_better_or_equal_everywhere() {
-    compare::<StdLibArcStrOnly, SStrBenchStr>(false);
-    compare::<StdLibStringOnly, SStrBenchStr>(false);
-    compare::<StdLibOptimized, SStrBenchStr>(false);
-    // note: 'bytes_reallocated' is worse here.
-    compare::<BytesBenchStr, SStrBenchStr>(true);
+    compare::<StdLibArcStrOnly, SStrBenchStr>(false, 0, "StdLibArcStrOnly");
+    compare::<StdLibStringOnly, SStrBenchStr>(false, 0, "StdLibStringOnly");
+    // exception: depending on the platform, the 'StdLibOptimized' is a bit better regarding to the number of re-allocations.
+    compare::<StdLibOptimized, SStrBenchStr>(false, 4, "StdLibOptimized");
+    // exception: 'bytes_reallocated' is worse here.
+    compare::<BytesBenchStr, SStrBenchStr>(true, 0, "BytesBenchStr");
 }
 
-fn compare<TBenchStr: BenchStr, TBetterBenchStr: BenchStr>(ignore_bytes_reallocated: bool) {
+fn compare<TBenchStr: BenchStr, TBetterBenchStr: BenchStr>(
+    ignore_bytes_reallocated: bool,
+    number_of_additional_re_allocations_allowed: usize,
+    vs_name: &str,
+) {
     let change = {
         let region = Region::new(GLOBAL);
         run_benchmark_single_threaded::<TBenchStr>(benchmark_test_set());
@@ -36,41 +43,48 @@ fn compare<TBenchStr: BenchStr, TBetterBenchStr: BenchStr>(ignore_bytes_realloca
 
     // the "better" should be better (or at least equal) everywhere.
     assert!(
-        change.reallocations >= change_better.reallocations,
-        "Expected to be better in number of re-allocations: {:?} (worse) vs. {:?} (better)",
+        change.reallocations + number_of_additional_re_allocations_allowed
+            >= change_better.reallocations,
+        "Expected to be better in number of re-allocations: {:?} ({}) vs. {:?} (better)",
         change,
+        vs_name,
         change_better
     );
     assert!(
         change.allocations >= change_better.allocations,
-        "Expected to be better in number of allocations: {:?} (worse) vs. {:?} (better)",
+        "Expected to be better in number of allocations: {:?} ({}) vs. {:?} (better)",
         change,
+        vs_name,
         change_better
     );
     assert!(
         change.deallocations >= change_better.deallocations,
-        "Expected to be better in number of de-allocations: {:?} (worse) vs. {:?} (better)",
+        "Expected to be better in number of de-allocations: {:?} ({}) vs. {:?} (better)",
         change,
+        vs_name,
         change_better
     );
     if !ignore_bytes_reallocated {
         assert!(
             change.bytes_reallocated >= change_better.bytes_reallocated,
-            "Expected to be better in bytes re-allocated: {:?} (worse) vs. {:?} (better)",
+            "Expected to be better in bytes re-allocated: {:?} ({}) vs. {:?} (better)",
             change,
+            vs_name,
             change_better
         );
     }
     assert!(
         change.bytes_allocated >= change_better.bytes_allocated,
-        "Expected to be better in bytes allocated: {:?} (worse) vs. {:?} (better)",
+        "Expected to be better in bytes allocated: {:?} ({}) vs. {:?} (better)",
         change,
+        vs_name,
         change_better
     );
     assert!(
         change.bytes_deallocated >= change_better.bytes_deallocated,
-        "Expected to be better in bytes de-allocated: {:?} (worse) vs. {:?} (better)",
+        "Expected to be better in bytes de-allocated: {:?} ({}) vs. {:?} (better)",
         change,
+        vs_name,
         change_better
     );
 }
